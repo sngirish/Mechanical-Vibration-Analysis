4b55dbdd284d6b9a21eb95c25ddd6939464387d8
sngirish <73772943+sngirish@users.noreply.github.com>
2020-11-01 22:07:57 +0530

    Update receptance_frf.py

--- receptance_frf.py
+++ receptance_frf.py
@@ -24,8 +24,12 @@ def plot_RFRF(w, r_amp, r_phase):
     fig, ax = plt.subplots(2, sharex = 'col', sharey = 'row')
     ax[0].plot(w, r_amp)
     ax[1].plot(w, r_phase)
-    ax[0].set(ylabel = 'Amplitude (m)', title = "Amplitude vs Frequency")
-    ax[1].set(ylabel = 'Phase angle (rad)', xlabel = 'Frequency (Hz)', title = "Phase angle vs Frequency", xticks = np.arange(0,11,1))
+    ax[0].set(ylabel = 'Amplitude (m/N)')
+    ax[1].set(ylabel = 'Phase angle (rad)', xlabel = 'Frequency (Hz)', \
+              title = "Phase angle vs Frequency", xticks = np.arange(0,11,1))
+
+    plt.text(0.1, 0.9, 'Receptance Amplitude', transform = ax[0].transAxes)
+    plt.text(0.1, 0.9, 'Receptance Phase', transform = ax[1].transAxes)
     plt.show()
     
 plot_RFRF(w, r_amp(k, m, zeta, w), r_phase(k, m, zeta, w))
